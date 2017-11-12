84bb8c26a8e95a67305d436d7c01ef688a9a3578
Syed Abdullah <contact@syedabdullah.net>
2017-11-12 23:48:26 +0800

    client/conn: add more methods for running PTC actions

--- client/connector.py
+++ client/connector.py
@@ -27,12 +27,12 @@ def create_user():
     r = requests.post(urljoin(API_URL, 'users'))
     return handle_json_response(r)
 
-def create_game(uid, num_cups, num_players, r, position):
+def create_game(uid, num_cups, num_players, rval, position):
     '''
     Creates a new cup shuffling game
     Returns: (salt, response)
     '''
-    salt, digest = get_commitment_digest(r, position)
+    salt, digest = get_commitment_digest(rval, position)
     payload = {
         'user_id': str(uid),
         'cups_count': num_cups,
@@ -52,12 +52,12 @@ def get_game(gid):
     r = requests.get(urljoin(API_URL, urn))
     return handle_json_response(r)
 
-def commit_game(gid, uid, r, guess):
+def commit_game(gid, uid, rval, guess):
     '''
     Bet on an ongoing cup shuffling game
     Returns: (salt, response)
     '''
-    salt, commit = get_commitment_digest(r, guess)
+    salt, commit = get_commitment_digest(rval, guess)
     urn = '/'.join(['games', str(gid), 'commit'])
 
     payload = {
@@ -67,3 +67,56 @@ def commit_game(gid, uid, r, guess):
 
     r = requests.post(urljoin(API_URL, urn), json=payload)
     return (salt, handle_json_response(r))
+
+def reveal_position(gid, uid, rval, position):
+    '''
+    Reveals the correct position of the cup
+    Returns: response from PTC
+    '''
+    urn = '/'.join(['games', str(gid), 'reveal'])
+    payload = {
+        'broker_id': uid,
+        'r': rval,
+        'position': position
+    }
+
+    r = requests.post(urljoin(API_URL, urn), json=payload)
+    return handle_json_response(r)
+
+def claim_winnings(gid, uid, rval):
+    '''
+    Claim winnings for particular user
+    '''
+    urn = '/'.join(['games', str(gid), 'claim'])
+    payload = {
+        'user_id': uid,
+        'r': rval
+    }
+
+    r = requests.post(urljoin(API_URL, urn), json=payload)
+    return handle_json_response(r)
+
+def forfeit_game(gid):
+    '''
+    Forfeits the game
+    '''
+    urn = '/'.join(['games', str(gid), 'forfeit'])
+    r = requests.post(urljoin(API_URL, urn))  
+    return handle_json_response(r)
+
+def refund_deposit(gid):
+    '''
+    Broker gets deposit
+    '''
+    urn = '/'.join(['games', str(gid), 'refund_deposit'])
+    r = requests.post(urljoin(API_URL, urn))  
+    return handle_json_response(r)
+    
+def advance_clock():
+    '''
+    Advances PTC clock
+    '''
+    urn = '/'.join(['clock', 'tick'])
+    r = requests.post(urljoin(API_URL, urn))  
+    return handle_json_response(r)
+    
